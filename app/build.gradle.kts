4b7c28c496a234557479151dccdaf2b5d95967e8
indra purnomo <indrapurnomoa@gmail.com>
2024-03-19 11:00:34 +0700

    add library API, Camera and Avatar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,21 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //API
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    //Camera
+    implementation ("androidx.camera:camera-camera2:1.4.0-alpha04")
+    implementation ("androidx.camera:camera-lifecycle:1.4.0-alpha04")
+    implementation ("androidx.camera:camera-view:1.4.0-alpha04")
+
+    //avatar
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
