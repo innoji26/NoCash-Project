7d25847a7908d42235b4c794c5f4e154da3b7909
zamzamnurahman <kazamnn1912@gmail.com>
2024-04-20 21:33:52 +0700

    refactor(): remove file redundant & change size icon splash

--- app/src/main/java/com/beranidigital/nocash/ui/pin/PinActivity.kt
+++ app/src/main/java/com/beranidigital/nocash/ui/pin/PinActivity.kt
@@ -1,11 +1,7 @@
 package com.beranidigital.nocash.ui.pin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.beranidigital.nocash.R
 import com.beranidigital.nocash.databinding.ActivityPinBinding
 
 class PinActivity : AppCompatActivity() {
