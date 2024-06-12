ef3726df76ee4c56595da622e48df450b319bf1a
innoji26 <indrapurnomoa@gmail.com>
2024-06-12 08:10:53 +0700

    add integration firebase to setData user in profile

--- app/src/main/java/com/beranidigital/nocash/ui/home/HomeFragment.kt
+++ app/src/main/java/com/beranidigital/nocash/ui/home/HomeFragment.kt
@@ -2,19 +2,38 @@ package com.beranidigital.nocash.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.beranidigital.nocash.data.model.UsersModel
 import com.beranidigital.nocash.databinding.FragmentHomeBinding
 import com.beranidigital.nocash.ui.home.promo.RecycleViewAdapterPromo
+import com.beranidigital.nocash.ui.login.LoginActivity
+import com.beranidigital.nocash.ui.main_navigation.MainHomeActivity
 import com.beranidigital.nocash.ui.profile.ProfileActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class HomeFragment : Fragment() {
 
@@ -22,8 +41,10 @@ class HomeFragment : Fragment() {
     private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
     private lateinit var promoRecyclerView: RecyclerView
-
     private lateinit var profileButton: ImageView
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var dbRef: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +63,20 @@ class HomeFragment : Fragment() {
 
         viewPager.adapter = HomeTabMenuAdapter(requireActivity())
 
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+        db = Firebase.database
+
+        if(firebaseUser == null){
+            //not signed in, launch the login activity
+            val newIntent = Intent(requireContext(), LoginActivity::class.java)
+            newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(newIntent)
+            return
+        }
+
+
+
 
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = when (position) {
@@ -53,12 +88,44 @@ class HomeFragment : Fragment() {
         profileNavigation()
 
         setupPromo()
+        setUserData()
+    }
+
+    private fun setUserData(){
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val userId = auth.uid ?: ""
+                dbRef = db.getReference("users").child(userId)
+
+                val snapshot = dbRef.get().await() // Menunggu hasil dari Firebase secara asinkron
+                if (snapshot.exists()) {
+                    val user = snapshot.getValue(UsersModel::class.java)
+                    if (user != null) {
+                        binding.tvName.text = user.name
+                    }
+                } else {
+                    Log.w(TAG, "Data tidak ditemukan")
+                    Toast.makeText(requireContext(), "Data pengguna tidak ditemukan", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                Log.w(TAG, "HomeFragment:failure", e)
+                updateUI(null)
+                Toast.makeText(requireContext(), "Gagal memuat data pengguna", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun profileNavigation(){
         profileButton.setOnClickListener {
             val newIntent = Intent(requireContext(), ProfileActivity::class.java)
             startActivity(newIntent)
+        }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?){
+        if(currentUser != null){
+            startActivity(Intent(requireContext(), MainHomeActivity::class.java))
+
         }
     }
 
@@ -69,5 +136,9 @@ class HomeFragment : Fragment() {
 
         val dummyPromo = listOf("Promo 1", "Promo 2", "Promo 3", "Promo 4", "Promo 5")
         promoRecyclerView.adapter = RecycleViewAdapterPromo(dummyPromo)
+    }
+
+    companion object{
+        private const val TAG = "HomeFragment"
     }
 }
--- app/src/main/java/com/beranidigital/nocash/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/beranidigital/nocash/ui/profile/ProfileFragment.kt
@@ -3,26 +3,39 @@ package com.beranidigital.nocash.ui.profile
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.beranidigital.nocash.R
+import com.beranidigital.nocash.data.model.UsersModel
 import com.beranidigital.nocash.databinding.FragmentProfileBinding
+import com.beranidigital.nocash.ui.home.HomeFragment
 import com.beranidigital.nocash.ui.login.LoginActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
     private lateinit var navController: NavController
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var dbRef: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +52,8 @@ class ProfileFragment : Fragment() {
 
         auth = Firebase.auth
         val firebaseUser = auth.currentUser
+        db = Firebase.database
+
 
         if(firebaseUser == null){
             //not signed in, launch the login activity
@@ -73,6 +88,30 @@ class ProfileFragment : Fragment() {
         }
 
         dialogLogout()
+        setUserData()
+    }
+
+    private fun setUserData(){
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val userId = auth.uid ?: ""
+                dbRef = db.getReference("users").child(userId)
+
+                val snapshot = dbRef.get().await() // Menunggu hasil dari Firebase secara asinkron
+                if (snapshot.exists()) {
+                    val user = snapshot.getValue(UsersModel::class.java)
+                    if (user != null) {
+                        binding.profileName.text = user.name
+                    }
+                } else {
+                    Log.w(TAG, "Data tidak ditemukan")
+                    Toast.makeText(requireContext(), "Data pengguna tidak ditemukan", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                Log.w(TAG, "ProfileFragment:failure", e)
+                Toast.makeText(requireContext(), "Gagal memuat data pengguna", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun dialogLogout(){
@@ -103,5 +142,9 @@ class ProfileFragment : Fragment() {
         val newIntent = Intent(requireContext(), LoginActivity::class.java)
         newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(newIntent)
+    }
+
+    companion object{
+        private const val TAG = "ProfileFragment"
     }
 }
